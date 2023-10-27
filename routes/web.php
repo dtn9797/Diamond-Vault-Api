e0e3015b0a95ad0523a1ddb071833275e2730b1c
shivanisureddy <52404943+shivani1105@users.noreply.github.com>
2023-10-27 11:56:03 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,20 @@ $router->post('product','App\Http\Controllers\ProductController@createProduct');
 $router->get('product/{id}','App\Http\Controllers\ProductController@updateProduct'); //for updating product
 $router->post('product/{id}','App\Http\Controllers\ProductController@deleteProduct');  // for deleting product
 $router->get('product','App\Http\Controllers\ProductController@index'); // for retrieving product
+
+// User registration
+$router->post('user','App\Http\Controllers\UserController@createUser');
+
+// User profile update
+$router->put('user/{id}', 'App\Http\Controllers\UserController@updateUser');
+
+// Password change
+$router->post('user/{id}/change-password', 'App\Http\Controllers\ChangePasswordController@changePassword');
+
+// Route to initiate the password reset process by creating a reset token
+$router->post('password/reset', 'App\Http\Controllers\PasswordResetController@createToken');
+
+// Route to handle password reset using a valid token
+$router->post('password/reset/{token}', 'App\Http\Controllers\PasswordResetController@resetPassword');
+
+$router->get('user','App\Http\Controllers\UserController@index');
