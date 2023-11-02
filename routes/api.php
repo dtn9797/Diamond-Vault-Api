afdbf6adcbedbfafd4f6ee4db4328c5fa1049a61
Shilpi98 <41938341+Shilpi98@users.noreply.github.com>
2023-11-02 17:16:29 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Product entry
+Route::post('product',[ProductController::class, 'createProduct']);
+
+//Product update
+Route::put('product/{id}',[ProductController::class,'updateProduct']);
+
+//Product delete
+Route::post('product/{id}',[ProductController::class,'deleteProduct']);
+
+Route::get('product/{id}',[ProductController::class,'getProduct']);
+
+Route::group(['middleware'=>['auth:sanctum']],function(){
+    Route::post('','');
+});
